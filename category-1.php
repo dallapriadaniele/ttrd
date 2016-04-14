59fe2020a7bb5b648e83fb29d41aa67dbb265681
Daniele Dalla Pria <dallapriadaniele@gmail.com>
2016-04-15 00:35:50 +0200

    .

--- category-1.php
+++ category-1.php
@@ -16,4 +16,5 @@
           <h1><?php printf( __( 'Category Archives: %s', 'bootstrapcanvaswp' ), '<span>' . single_cat_title( '', false ) . '</span>' ); ?></h1>
             ewq
 asdsa
+            dsadsa
      
