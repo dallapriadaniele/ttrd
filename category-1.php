d5dfaac053102a95b83b064a0ae0fd40d7975018
Daniele Dalla Pria <dallapriadaniele@gmail.com>
2016-04-15 00:42:57 +0200

    .

--- category-1.php
+++ category-1.php
@@ -7,21 +7,3 @@
  */
 
 	get_header(); ?>
-
-      <div class="row">
-
-        <div class="col-sm-8 blog-main">
-           dsadsadsadsasadsadsa dsada
-
-          <h1><?php printf( __( 'Category Archives: %s', 'bootstrapcanvaswp' ), '<span>' . single_cat_title( '', false ) . '</span>' ); ?></h1>
-            ewq
-		  <hr />
-		  <?php get_template_part( 'loop', 'category' ); ?>
-            dsadsadsadsa
-
-        </div><!-- /.blog-main -->
-
-        <?php get_sidebar(); ?>
-
-      </div><!-- /.row -->
-      
